0f0222cd6b5017d48f78b723524f15d419baf955
Patrick <ctrlaltpat@gmail.com>
2018-10-18 09:46:40 +0100

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @comment = @post.comments.build
   end
 
   def index
@@ -16,9 +17,25 @@ class PostsController < ApplicationController
     redirect_to post
   end
 
+  # def update
+  #   # byebug
+  #   @post = Post.find(params[:id])
+  #   new_comment = @post.comments.build(
+  #       content: params[:comment_content], 
+  #       user_id: params[:user_id]
+  #     )
+  #   @post.save
+  #   byebug
+  #   redirect_to post_path(@post)
+  # end
+
   private
 
   def post_params
     params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
   end
+
+  # def comment_params
+  #   params.require(:post).permit(:id, :content, :user_id)
+  # end
 end
